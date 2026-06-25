c4f68b8439c4027caf5c7745a9535d1cdf714f5b
HakuRan <WBhlietue@gmail.com>
2026-06-25 20:13:01 +0800

    fix??

--- seewk.bat
+++ seewk.bat
@@ -8,13 +8,25 @@ if "%IS_INIT%"=="" (
     set "initURL=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
     call "!initURL!" x64
     set IS_INIT=1
+    if "%1"=="start" goto startProjInit
+    if "%1"=="make" goto makeInit
     cmd /k
     endlocal
-    goto :EOF
 )
-
+chcp 65001
 if "%1"=="start" goto startProj
 if "%1"=="make" goto make
+goto :EOF
+
+:startProjInit
+ninja -C build && build\Test.exe
+cmd /k
+goto :EOF
+
+:makeInit
+call cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=%vclocation% -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release -G Ninja
+cmd /k
+goto :EOF
 
 :startProj
 ninja -C build && build\Test.exe
@@ -22,6 +34,6 @@ goto :EOF
 
 :make
 
-call cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=%vclocation% -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_BUILD_TYPE=Release -G Ninja
+call cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=%vclocation% -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release -G Ninja
 goto :EOF
 
