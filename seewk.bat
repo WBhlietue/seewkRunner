b08627931ae660e5375d5030b2348103bd12178e
HakuRan <WBhlietue@gmail.com>
2026-07-07 16:47:14 +0800

    change

--- seewk.bat
+++ seewk.bat
@@ -1,62 +1,71 @@
 @echo off
 
-@REM set "SEEWK_DIR=%~dp0"
-@REM cd /d "%SEEWK_DIR%"
-@REM set VCPKG_ROOT="%~dp0vcpkg\scripts\buildsystems\vcpkg.cmake"
-if "%VCPKG_ROOT%"=="" set VCPKG_ROOT=%~dp0vcpkg
+
+
+if "%VCPKG_ROOT%"=="" (
+    set "vcpkg_path="
+    for /f "delims=" %%i in ('where vcpkg 2^>nul') do (
+        set "vcpkg_path=%%i"
+        goto :found
+    )
+    :found
+    if defined vcpkg_path (
+        for %%a in ("%vcpkg_path%") do set "VCPKG_ROOT=%%~dpa"
+    ) else (
+        set VCPKG_ROOT=%~dp0vcpkg
+    )
+)
 
 if "%IS_INIT%"=="" (
     setlocal enabledelayedexpansion
+    chcp 65001
     set "initURL=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
     call "!initURL!" x64
-    set IS_INIT=1
-    if "%1"=="start" goto startProjInit
-    if "%1"=="make" goto makeInit
-    if "%1"=="run" goto runInit
-    if "%1"=="compile" goto compileInit
+    
+    if "%1"=="start" goto startProj
+    if "%1"=="make" goto make
+    if "%1"=="run" goto run
+    if "%1"=="compile" goto compile
     cmd /k
     endlocal
 )
-chcp 65001
+
 if "%1"=="start" goto startProj
 if "%1"=="run" goto run
 if "%1"=="make" goto make
 if "%1"=="compile" goto compile
+
+echo command not found
 goto :EOF
 
-:startProjInit
-ninja -C build && build\Test.exe
-cmd /k
-goto :EOF
+:init 
+if "%IS_INIT%" == "" (
+    setlocal enabledelayedexpansion
+    set IS_INIT=1
+    cmd /k
+    endlocal
+)
+goto :EOF 
 
-:makeInit
-call cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release -G Ninja
-cmd /k
-goto :EOF
+
 
 :startProj
 ninja -C build && build\Test.exe
-goto :EOF
+goto init
 
 :compile
 ninja -C build
-goto :EOF
-:compileInit
-ninja -C build
-cmd /k
-goto :EOF
+goto init
+
 
 
 :run
-build\Test.exe 
-goto :EOF
-:runInit
-build\Test.exe 
-cmd /k
-goto :EOF
+goto init
+
 
 :make
+call cmake -B build -S . -G Ninja
+@REM call cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release -G Ninja
+goto init
 
-call cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release -G Ninja
-goto :EOF
 
